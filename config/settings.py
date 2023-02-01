2d1ebfc1ef42e5032990de7f26a519b45aa40ec4
Dyno <0xDyno@gmail.com>
2023-02-01 17:38:26 +0800

    set up to hande static files after collect static

--- config/settings.py
+++ config/settings.py
@@ -124,9 +124,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "webstat/",
-]
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "webstat/"]
+else:
+    STATIC_ROOT = BASE_DIR / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
